a4ddb0179aa90cb038b7aa567b52aa4e078c0806
Jazzpirate <raupi@jazzpirate.com>
2022-09-05 08:47:49 +0200

    temp

--- src/main/scala/info/kwarc/rustex/bridge.scala
+++ src/main/scala/info/kwarc/rustex/bridge.scala
@@ -6,7 +6,6 @@ import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
 import java.io.{File, FileInputStream, FileOutputStream}
 import java.net.URI
 import java.util
-import scala.collection.mutable
 
 object Implicits {
   import scala.jdk.CollectionConverters._
@@ -16,13 +15,9 @@ object Implicits {
   }
 }
 import Implicits._
-private class Bridge {
-  @native def initialize(path:String) : Boolean
-  @native def parse(file:String,p:Params,memories:util.ArrayList[String]) : String
-  @native def parseString(file: String, text:String, p: Params, memories: util.ArrayList[String]): String
-}
+
 abstract class Params {
-  var singlethreaded = false
+  var singlethreaded = true
   var do_log = false
   var store_in_file = false
   var copy_tokens_full = true
@@ -41,22 +36,70 @@ abstract class Params {
   }
   def error(msg:String,stacktrace:List[(String,String)],files:List[(String,Int,Int)])
 }
-/*
 
-class JInterpreter {
-  private var pointer : Long = 0
+object RusTeXBridge {
+  private var main_bridge: Option[RusTeXBridge] = None
 
-  @native def jobname() : String
-}
-abstract class JExecutable(val name : String) {
-  def execute(_int: JInterpreter) : Boolean
-}
+  def initialized = main_bridge.isDefined
 
- */
+  def mainBridge = main_bridge.get
 
+  def initialize(path: String) = {
+    val actualpath = path + "/" + library_filename()
+    val file = new File(actualpath)
+    if (!file.exists()) {
+      download("https://github.com/slatex/RusTeX/releases/download/latest/" + library_filename(), actualpath)
+    }
+    System.load(actualpath)
+    val pdffile = new File(path + "/" + library_filename("pdfium"))
+    if (!pdffile.exists()) {
+      val tgzname = {
+        val syspath = System.getProperty("os.name").toUpperCase()
+        "pdfium-" + (if (syspath.startsWith("WINDOWS")) "win"
+        else if (syspath.startsWith("MAC")) "mac"
+        else "linux") + "-x64.tgz"
+      }
+      val tgzpath = path + "/" + tgzname
+      download("https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F5145/" + tgzname, tgzpath)
+      val tgf = new File(tgzpath)
+      val filein = new FileInputStream(tgf)
+      val in = new TarArchiveInputStream(new GzipCompressorInputStream(filein))
+      var next = in.getNextEntry
+      var done = false
+      try {
+        while (next != null && !done) {
+          if (next.getName.endsWith(library_filename("pdfium"))) {
+            val out = new FileOutputStream(pdffile)
+            val bytearray = LazyList.continually(in.read()).takeWhile(_ != -1).map(_.toByte).toArray
+            out.write(bytearray)
+            out.close()
+            done = true
+          } else next = in.getNextEntry
+        }
+      } finally {
+        in.close()
+        tgf.delete()
+      }
+    }
+    val b = new RusTeXBridge() {
+      override def parse(file: String): String = parseI(ptr, params, file, memories, true)
+      override def parseString(file: String, text: String): String = parseStringI(ptr, params, text, file, memories, true)
+      override private[rustex] def initialize: Unit = {
+        initializeMain(path + "/")
+        super.initialize
+      }
+    }
+    main_bridge = Some(b)
+  }
 
-object Bridge {
-  def download(uri:String,filestr:String) : Unit = {
+  private def library_filename(libname: String = "rustex_java") = {
+    val syspath = System.getProperty("os.name").toUpperCase()
+    if (syspath.startsWith("WINDOWS")) libname + ".dll"
+    else if (syspath.startsWith("MAC")) "lib" + libname + ".dylib"
+    else "lib" + libname + ".so"
+  }
+
+  private def download(uri: String, filestr: String): Unit = {
     val url = new URI(uri).toURL
     val conn = url.openConnection()
     val httpConn = conn.asInstanceOf[java.net.HttpURLConnection]
@@ -64,7 +107,7 @@ object Bridge {
     // setFollowRedirects does not actually follow redirects
     if (resp.toString.startsWith("30")) {
       val redirectURL = conn.getHeaderField("Location")
-      download(redirectURL,filestr)
+      download(redirectURL, filestr)
     }
     else if (!resp.toString.startsWith("40")) {
       val in = conn.getInputStream
@@ -80,103 +123,100 @@ object Bridge {
       }
     }
   }
-  //System.load("/home/jazzpirate/work/Software/RusTeX/rustexbridge/target/debug/librustex_java.so")
-  private var bridge : Option[Bridge] = None
-  def initialize(path : String): Unit = {
-    val actualpath = path + "/" + library_filename()
-    val file = new File(actualpath)
-    if (!file.exists()) {
-      download("https://github.com/slatex/RusTeX/releases/download/latest/" + library_filename(),actualpath)
-    }
-    System.load(actualpath)
-    val pdffile = new File(path + "/" + library_filename("pdfium"))
-    if (!pdffile.exists()) {
-      val tgzname = {
-        val syspath = System.getProperty("os.name").toUpperCase()
-        "pdfium-" + (if (syspath.startsWith("WINDOWS")) "win"
-        else if (syspath.startsWith("MAC")) "mac"
-        else "linux") + "-x64.tgz"
-      }
-      val tgzpath = path + "/" + tgzname
-      download("https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F5145/" + tgzname,tgzpath)
-      val tgf = new File(tgzpath)
-      val filein = new FileInputStream(tgf)
-      val in = new TarArchiveInputStream(new GzipCompressorInputStream(filein))
-      var next = in.getNextEntry
-      var done = false
-      try { while (next != null && !done) {
-        if (next.getName.endsWith(library_filename("pdfium"))) {
-          val out = new FileOutputStream(pdffile)
-          val bytearray = LazyList.continually(in.read()).takeWhile(_ != -1).map(_.toByte).toArray
-          out.write(bytearray)
-          out.close()
-          done = true
-        } else next = in.getNextEntry
-      } } finally {
-        in.close()
-        tgf.delete()
-      }
-    }
-    val b = new Bridge
-    b.initialize(path + "/")
-    bridge = Some(b)
-    //println("TeX Engine Initialized")
-  }
-  def parse(s : String,p:Params, memories:List[String] = Nil) = bridge match {
-    case Some(b) => b.parse(s,p,Implicits(memories))
-    case None => ???
-  }
 
-  def parseString(file:String,s: String, p: Params, memories: List[String] = Nil) = bridge match {
-    case Some(b) => b.parseString(file, s, p, Implicits(memories))
-    case None => ???
+  val noParams = new Params {
+    override def log(s: String): Unit = {}
+    override def write_16(s: String): Unit = {}
+    override def write_17(s: String): Unit = {}
+    override def write_18(s: String): Unit = {}
+    override def write_neg_1(s: String): Unit = {}
+    override def write_other(s: String): Unit = {}
+    override def file_open(s: String): Unit = {}
+    override def file_close(): Unit = {}
+    override def message(s: String): Unit = {}
+    override def error(msg: String, stacktrace: List[(String, String)], files: List[(String, Int, Int)]): Unit = {}
   }
-  def library_filename(libname:String = "rustex_java") = {
-    val syspath = System.getProperty("os.name").toUpperCase()
-    if (syspath.startsWith("WINDOWS")) libname + ".dll"
-    else if (syspath.startsWith("MAC")) "lib" + libname + ".dylib"
-    else "lib" + libname + ".so"
-  }
-  def initialized() = bridge.isDefined
-  //System.load("/home/jazzpirate/work/Software/RusTeX/librustex.so")
-  //private val bridge = new Bridge
-  //bridge.initialize()
-  /*def test() = {
-    bridge.test(new JExecutable("pdfoutput") {
-      override def execute(_int: JInterpreter): Boolean = {
-        println("Fuck yeah!" + _int.jobname())
-        sys.exit()
-        true
-      }
-    } :: Nil)
-  }*/
 }
 
+class RusTeXBridge(private[rustex] var params: Params = RusTeXBridge.noParams, protected var memories: List[String] = Nil) {
+  private[rustex] var ptr: Long = 0
+
+  @native private def newsb(): Unit
+  @native private[rustex] def parseI(ptr: Long, p: Params, file: String, memories: util.ArrayList[String], use_main: Boolean): String
+  @native private[rustex] def parseStringI(ptr: Long, p: Params, text: String, file: String, memories: util.ArrayList[String], use_main: Boolean): String
+  @native private[rustex] def initializeMain(path: String): Boolean
+
+  def setParams(p: Params) = params = p
+  def setMemories(mems: List[String]) = memories = mems
+  def parse(file: String) = parseI(ptr, params, file, memories, false)
+  def parseString(file: String, text: String) = parseStringI(ptr, params, text, file, memories, false)
+
+  private[rustex] def initialize {
+    newsb()
+  }
+  initialize
+}
 
 object Test {
   def main(args: Array[String]): Unit = {
     val testparams = new Params {
       override def log(s: String): Unit = println("LOG: " + s)
+
       override def write_16(s: String): Unit = println("WRITE16: " + s)
+
       override def write_17(s: String): Unit = println("WRITE17: " + s)
+
       override def write_18(s: String): Unit = println("WRITE18: " + s)
+
       override def write_neg_1(s: String): Unit = println("WRITE-1: " + s)
+
       override def write_other(s: String): Unit = println("OTHER: " + s)
+
       override def file_open(s: String): Unit = println("FILE OPEN: " + s)
+
       override def file_close(): Unit = println("FILE CLOSE")
+
       override def message(s: String): Unit = println("MSG: " + s)
 
-      override def error(msg: String, stacktrace: List[(String, String)],files : List[(String,Int,Int)]): Unit =
-        println("Error: " + msg + "\n\n" + stacktrace.map{case (a,b) => a + " - " + b}.mkString("\n") + "\n\n" +
-          files.map{case (s,l,p) => s + "(" + l + "," + p + ")"}.mkString("\n")
+      override def error(msg: String, stacktrace: List[(String, String)], files: List[(String, Int, Int)]): Unit =
+        println("Error: " + msg + "\n\n" + stacktrace.map { case (a, b) => a + " - " + b }.mkString("\n") + "\n\n" +
+          files.map { case (s, l, p) => s + "(" + l + "," + p + ")" }.mkString("\n")
         )
     }
-    //Bridge.initialize("/Users/dennismuller/work/RusTeX/rustexbridge/target")
-    Bridge.initialize("/home/jazzpirate/work/Software/RusTeX/rustexbridge/target/x86_64-unknown-linux-gnu/release")
+    val tp2 : Params = new Params {
+      override def log(s: String): Unit = {}
+      override def write_16(s: String): Unit = {}
+      override def write_17(s: String): Unit = {}
+      override def write_18(s: String): Unit = {}
+      override def write_neg_1(s: String): Unit = {}
+      override def write_other(s: String): Unit = {}
+      override def file_open(s: String): Unit = if (s.contains("smglom")) println(s.trim)
+      override def file_close(): Unit = {}
+      override def message(s: String): Unit = {}
+      override def error(msg: String, stacktrace: List[(String, String)], files: List[(String, Int, Int)]): Unit = {}
+
+    }
+    RusTeXBridge.initialize("/home/jazzpirate/work/Software/sTeX/RusTeX/rustexbridge/target/x86_64-unknown-linux-gnu/release")
     //val ret = Bridge.parse("/home/jazzpirate/work/LaTeX/Others/test.tex",testparams)
     println("jupyterNB.en.tex")
-    Bridge.parse("/home/jazzpirate/work/MathHub/smglom/IWGS/source/jupyterNB.en.tex",testparams,List("c_stex_module"))
+    RusTeXBridge.mainBridge.setParams(tp2)
+    RusTeXBridge.mainBridge.setMemories(List("c_stex_module"))
+    RusTeXBridge.mainBridge.parse("/home/jazzpirate/work/MathHub/smglom/IWGS/source/jupyterNB.en.tex")
     println("BBPformula")
+    val sb = new RusTeXBridge(tp2,List("c_stex_module")) //Sandbox.construct(testparams,List("c_stex_module"))
+    sb.parse("/home/jazzpirate/work/MathHub/smglom/analysis/source/BBPformula.en.tex")
+    println("alternatingharmonic")
+    sb.parse("/home/jazzpirate/work/MathHub/smglom/analysis/source/alternatingharmonicseries.en.tex")
+    println("asymptotic")
+    sb.parse("/home/jazzpirate/work/MathHub/smglom/analysis/source/asymptoticdensity.en.tex")
+    println("BBPformula")
+    sb.parse("/home/jazzpirate/work/MathHub/smglom/analysis/source/BBPformula.en.tex")
+    println("jupyterNB.en.tex 1...")
+    RusTeXBridge.mainBridge.parse("/home/jazzpirate/work/MathHub/smglom/IWGS/source/jupyterNB.en.tex")
+    println("jupyterNB.en.tex 2...")
+    sb.parse("/home/jazzpirate/work/MathHub/smglom/IWGS/source/jupyterNB.en.tex")
+    println("Done.")
+    /*
     Bridge.parse("/home/jazzpirate/work/MathHub/smglom/analysis/source/BBPformula.en.tex",testparams,List("c_stex_module"))
     println("alternatingharmonic")
     Bridge.parse("/home/jazzpirate/work/MathHub/smglom/analysis/source/alternatingharmonicseries.en.tex",testparams,List("c_stex_module"))
@@ -204,5 +244,7 @@ object Test {
     Bridge.parse("/home/jazzpirate/work/MathHub/smglom/analysis/source/hyperboliccosineintegral.en.tex",testparams,List("c_stex_module"))
     println("Done")
     //println(ret)
+
+ */
   }
 }
